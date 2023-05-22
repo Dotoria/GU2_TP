6263c563b665030d42c3efceab92f35f9f9ae640
Jwknm <116863340+Jwknm@users.noreply.github.com>
2023-05-22 19:15:49 +0900

    Update AGN_SAMI.py

--- TP_KSH/AGN_SAMI.py
+++ TP_KSH/AGN_SAMI.py
@@ -8,7 +8,7 @@ import astropy.io.fits as fits
 import spectral as sp
 import spectral.io.aviris as aviris
 from PyAstronomy import pyasl
-# GAMA, cluster IDs
+
 GAMAid = list(SAMI.GAMAdata['CATID'])
 clustid = list(SAMI.clustdata['CATID'])
 CATID = GAMAid + clustid
@@ -16,7 +16,6 @@ CATID.sort()
 #print(len(CATID))
 
 def SAMI_spectra(catid):
-    # loading data
     mypath = './sami/dr3/ifs/'
     vel = '_A_stellar-velocity_default_two-moment.fits'
     disp = '_A_stellar-velocity-dispersion_default_two-moment.fits'
@@ -48,8 +47,6 @@ def SAMI_spectra(catid):
     S6731data = dat.removeNan(S6731data)
     S6716data = dat.removeNan(S6716data)
     return Hadata, Hbdata, N2data, O1data, O2data, O3data, S6731data, S6716data
-
-
 HaList = []
 HbList = []
 O3List = []
@@ -60,31 +57,89 @@ AGN = []
 for id in CATID:
     try :
         Hadata, Hbdata, N2data, O1data, O2data, O3data, S6731data, S6716data = SAMI_spectra(int(id))
-        Ha = np.max(Hadata)
-        Hb = np.max(Hbdata)
-        O3 = np.max(O3data)
-        N2 = np.max(N2data)
+
+        Ha = np.nanmax(Hadata)
+        Hb = np.nanmax(Hbdata)
+        O3 = np.nanmax(O3data)
+        N2 = np.nanmax(N2data)
+
         HaList.append(Ha)
         HbList.append(Hb)
         O3List.append(O3)
         N2List.append(N2)
-        if O3/Hb > 3 and N2/Ha > 0.6 :
-            Seyfert.append(id)
+        if np.log10(O3/Hb) > 0.61/(np.log10(N2/Ha)-0.05) + 1.3:
+
+            AGN.append(int(id))
         elif O3/Hb < 3 and N2/Ha > 0.6 :
-            LINER.append(id)
-        elif O3/Hb > 0.61/(np.log10(N2/Ha)-0.05)+1.3:
-            AGN.append(id)
+            LINER.append(int(id))
+        elif O3/Hb > 3 and N2/Ha > 0.6 :
+            Seyfert.append(int(id))
     except(ValueError) : continue
 
-plt.scatter(np.log10(np.array(N2List)/np.array(HaList)), np.log10(np.array(O3List)/np.array(HbList)),s=0.5,c='black')
+def view(catid):
+        mypath = './sami/dr3/ifs/'
+        Halpha = '_A_Halpha_default_recom-comp.fits'
+        Hbeta = '_A_Hbeta_default_recom-comp.fits'
+        N2 = '_A_NII6583_default_recom-comp.fits'
+        O1 = '_A_OI6300_default_recom-comp.fits'
+        O2 = '_A_OII3728_default_recom-comp.fits'
+        O3 = '_A_OIII5007_default_recom-comp.fits'
+        S6716 = '_A_SII6716_default_recom-comp.fits'
+        S6731 = '_A_SII6731_default_recom-comp.fits'
+        Hadata = fits.open(mypath + '{}'.format(catid) + '/' + '{}'.format(catid) + Halpha)[0].data
+        Hbdata = fits.open(mypath + '{}'.format(catid) + '/' + '{}'.format(catid) + Hbeta)[0].data
+        N2data = fits.open(mypath + '{}'.format(catid) + '/' + '{}'.format(catid) + N2)[0].data
+        O1data = fits.open(mypath + '{}'.format(catid) + '/' + '{}'.format(catid) + O1)[0].data
+        O2data = fits.open(mypath + '{}'.format(catid) + '/' + '{}'.format(catid) + O2)[0].data
+        O3data = fits.open(mypath + '{}'.format(catid) + '/' + '{}'.format(catid) + O3)[0].data
+        S6731data = fits.open(mypath + '{}'.format(catid) + '/' + '{}'.format(catid) + S6731)[0].data
+        S6716data = fits.open(mypath + '{}'.format(catid) + '/' + '{}'.format(catid) + S6716)[0].data
+
+        plt.subplot(3 ,3, 1)
+        plt.imshow(Hadata[0])
+        plt.title('Ha')
+
+        plt.subplot(3, 3, 2)
+        plt.imshow(Hbdata)
+        plt.title('Hb')
+
+        plt.subplot(3, 3, 3)
+        plt.imshow(N2data)
+        plt.title('N2')
+
+        plt.subplot(3, 3, 4)
+        plt.imshow(O1data)
+        plt.title('O1')
+
+        plt.subplot(3, 3, 5)
+        plt.imshow(O2data)
+        plt.title('O2')
+
+        plt.subplot(3, 3, 6)
+        plt.imshow(O3data)
+        plt.title('O3')
+
+        plt.subplot(3, 3, 7)
+        plt.imshow(S6731data)
+        plt.title('S6731')
+
+        plt.subplot(3, 3, 8)
+        plt.imshow(S6716data)
+        plt.title('S6716')
+        plt.legend(str(catid))
+        plt.show()
+
+
+print(len(AGN))
+
+for id in AGN:
+    view(id)
+    # ra=SAMI.GAMAdata['ra']['CATID'==id]
+    # dec=SAMI.GAMAdata['dec']['CATID' == id]
+    # print(id, ra, dec)
+
+"""plt.scatter(np.log10(np.array(N2List)/np.array(HaList)), np.log10(np.array(O3List)/np.array(HbList)),s=0.5,c='black')
 plt.axhline(np.log10(3))
 plt.axvline(np.log10(0.6))
-#plt.plot(np.log10(np.array(N2List)/np.array(HaList)),0.61/(np.log10(np.array(N2List)/np.array(HaList))-0.05)+1.3,'--')
 plt.show()
-
-print(AGN)
-
-# img = sp.open_image()
-# print(img)
-
-#img.bands = aviris.read_aviris_bands('92AV3C.spc')
+print(AGN,len(AGN))"""
